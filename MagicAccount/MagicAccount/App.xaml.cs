f74c146f59100d690246f2837ee2293fafbfbb78
JaydipGarala <jaydipkumargarala@gmail.com>
2020-11-22 10:09:52 +0530

    Change Label

---
+++ MagicAccount/MagicAccount/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using MagicAccount.Pages;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MagicAccount
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            //MainPage = new MainPage();
+            MainPage = new LoginPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
